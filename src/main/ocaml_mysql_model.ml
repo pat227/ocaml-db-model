3136fac19b940bdc40ce197a2bedda64a2e36bee
paul <paul@guerriere>
2018-04-12 09:18:34 -0400

    Drafting alterntive argument parsing not using core.  Changes to be committed: 	modified:   src/main/ocaml_mysql_model.ml

--- src/main/ocaml_mysql_model.ml
+++ src/main/ocaml_mysql_model.ml
@@ -2,12 +2,12 @@ module Credentials = Credentials2copy.Credentials
 module Utilities = Utilities2copy.Utilities
 module Model = Model.Model
 module Sql_supported_types = Sql_supported_types.Sql_supported_types
-open Core
 module Command = struct
 
   let execute regexp_opt table_list_opt ppxlist_opt
 	      module_names where2find_modules
 	      sequoia host user password database () =
+    let open Core in 
     let open Core.Result in
     (*==todo==refactor the below into a functon in model.ml*)
     try
@@ -54,7 +54,7 @@ module Command = struct
           project for building, and allow user to tweak the utilities file.--*)
     with
     | Failure s -> Utilities.print_n_flush s
-
+(*
   let main_command =
     let open Core.Command in
     Core.Command.basic
@@ -68,7 +68,7 @@ module Command = struct
 			 +> flag "-table-regexp" (optional string)
 				 ~doc:"Only model those tables that match a regexp."
 			 +> flag "-table-list" (optional string)
-				 ~doc:"Csv-with-no-spaces table-name list"
+				 ~doc:"Csv-with-no-spaces table-name list."
 			 +> flag "-ppx-extensions" (optional string) 
 				 ~doc:"Comma seperated list of ppx extensions; \
 				       currently support fields, show, sexp, \
@@ -93,6 +93,36 @@ module Command = struct
 			 +> flag "-password" (required string) ~doc:"db password."
 			 +> flag "-db" (required string) ~doc:"db name."
 			) execute;;
+ *)
+
+  let main_command_nocore =
+    let usagemsg = "Connect to a mysql db, get schema, write modules and (primitive) \
+		    types out of thin air with ppx extensions and a utility module  \
+		    for parsing mysql strings into present directory. Use basic \
+		    regexp, or a list, to filter table names." in
+    let execute_namedargs ~regexp_opt ~table_list_opt ~ppxlist_opt
+			  ~module_names ~where2find_modules
+			  ~sequoia ~host ~user ~password ~database () =
+      match host, user, password, database with
+      | None, _, _, _ 
+      | _, None, _, _ 
+      | _, _, None, _, _ 
+      | _, _, _, None -> Utilities.print_n_flush "Host, user, password, and database name are required args."
+      | Some h, Some u, Some pwd, Some db ->
+	 execute regexp_opt table_list_opt ppxlist_opt module_names where2fine_modules sequoia h u pwd db () in
+    let command_ref = ref execute_namedargs in 
+    let speclist = [("-host", Arg.String (fun s -> command_ref := !command_ref ~host:s) ,"ipv4 of the db host.");
+		    ("-user",Arg.String (fun s -> command_ref := !command_ref ~user:s),"db user.");
+		    ("-password",Arg.String (fun s -> command_ref := !command_ref ~password:s),"db password.");
+		    ("-db",Arg.String (fun s -> command_ref := !command_ref ~database:s),"db name.");
+		    ("table-regexp",,"Only model those tables that match a regexp.");
+		    ("table-list",,"Csv-with-no-spaces table-name list.");
+		    ("ppx-extensions",,"Comma seperated list of ppx extensions; currently support fields, show, sexp, ord, eq, yojson, which are also defaults.");
+		    ("module-field-types",,"Force any db fields whose name matches any in the csv-with-no-spaces list to not be a primitive, but instead a type defined in a module of the same name. A directory must be provided where to find the source ml files for each in another (the next) arg. The name should not be sans suffix, ie, without \".ml\" or \".mli\"");
+		    ("path-to-modules",,"Absolute path to the directory within the project that contains any modules (mli files) specified by module-field-types arg.");
+		    ("-sequoia",,"Support for sequoia: optionally output modules suitable for use with the Sequoia library.")
+		   ] in
+    !command_ref ();;
     
   let () =
     let open Core.Command in
